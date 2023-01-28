98e1bd2e407694e26f0731d3a21b8773d2f9d877
abdurahmanjabiin <jabiin626@gmail.com>
2023-01-28 19:33:47 +0300

    starter pack added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+    <div className="container">
+    <div className='logo'> Logo</div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li> content</li>
+      </ul>
+    </div>
+    </div>
+    
+  );
+}
+
+
+
+export default App;
