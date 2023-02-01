fa9930519a45ab6b35eac8e812ddfe5ae5bd1771
Abdu-rahman <jabiin626@gmail.com>
2023-02-01 18:42:07 +0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="App">
     <div className="container">
-    <div className='logo'> Logo</div>
+    <div className='logo'> jsx test </div>
       <ul>
         
       </ul>
